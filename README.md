8ea7f50b8c8a700ae724a1f0ea2e0c39e009e6b6
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-25 18:22:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# brithday-reminder
+## Idea
+
+[https://uidesigndaily.com/](https://uidesigndaily.com/posts/sketch-birthdays-list-card-widget-day-1042)
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    name: 'Marta Stock',
+    age: 27,
+    image:
+      'https://www.mariuszbrainard.de/.cm4all/uproc.php/0/CHARCOAL_KOHLE/.m.brainard_charcoal_052.jpg/picture-2600?_=164188a7af0',
+  },
+  {
+    id: 2,
+    name: 'Eva Summer',
+    age: 31,
+    image:
+      'https://www.mariuszbrainard.de/.cm4all/uproc.php/0/OIL_PASTELL/.m.brainard_oil_pastell_025.jpg/picture-2600?_=16419126000',
+  },
+  {
+    id: 3,
+    name: 'John Jobs',
+    age: 29,
+    image:
+      'https://www.mariuszbrainard.de/.cm4all/uproc.php/0/CHARCOAL_KOHLE/.m.brainard_charcoal_013.jpg/picture-2600?_=164188a7af0',
+  },
+  {
+    id: 4,
+    name: 'Paul Fish',
+    age: 34,
+    image:
+      'https://www.mariuszbrainard.de/.cm4all/uproc.php/0/CHARCOAL_KOHLE/.m.brainard_charcoal_006.jpg/picture-2600?_=164188a7af0',
+  },
+  {
+    id: 5,
+    name: 'Katrin Sommer',
+    age: 29,
+    image:
+      'https://www.mariuszbrainard.de/.cm4all/uproc.php/0/CHARCOAL_KOHLE/.m.brainard_charcoal_026.jpg/picture-2600?_=164188a7af0',
+  },
+];
+
